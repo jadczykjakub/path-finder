bf8d81710ea9530a2ac0f33e3bfc0358e7c89f2b
jadj <jakub.jadczyk@innsoft.pl>
2024-08-26 10:05:04 +0200

    fix css and metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import MazeGrid from "./path-finder/components/MazeGrid";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between px-8 py-8 md:py-16">
     <div className=" grid gap-8 justify-items-center  ">
       <h1 className='text-2xl font-bold text-center'>Visual presentation of 2 algorithms</h1>
 
